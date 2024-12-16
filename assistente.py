ff69681a226e96845845b05ffe38e75976479aac
Valesca M Oliveira <62262963+valescamelo@users.noreply.github.com>
2024-12-16 09:13:28 -0300

    função para informar hora e realizar pesquisa

--- assistente.py
+++ assistente.py
@@ -1,6 +1,8 @@
 #etapas do criação do programa assistente virtual 
 import speech_recognition as sr
 import pyttsx3
+import webbrowser
+from datetime import datetime
 
 def falar(texto): #transforma texto em fala
     enige = pyttsx3.init() #pyttsx3 sintetiza voz 
@@ -21,7 +23,17 @@ def ouvir(): #verifica ou audio do microfone e converte em texto
             return "Erro no serviço de reconhecimento de voz."
         except sr.WaitTimeoutError:
             return "Você não informou nada. Tempo egostado!"
-        
+
+def consultar_hr(): #função para consultar hora no assistente virtual
+    agora = datetime.now()
+    hora_atual = agora.strftime("%H:%M") #strftime personalisa horas e datas 
+    return (f"Informações de hora: {hora_atual}")
+
+def pesquisar_google(termo): #função para abrir uma pesquisa no navegador
+    url = (f"https://www.google.com/search?q={termo}")
+    webbrowser.open(url)
+    return (f"Pesquisa encontrada conforme solicitado: {termo}.")
+           
 def assistente(): #principal função do assistente virtual
     falar('Olá! Sou sua assistente virtual. No que posso ajudar?')
 
@@ -36,6 +48,17 @@ def assistente(): #principal função do assistente virtual
             falar('Olá! Como posso ajudar você?')
         elif "está tudo bem?" in informacao:
             falar('Estou bem, obrigada!')
+        elif "me informe a hora" in informacao:
+            hora = consultar_hr
+            falar(hora)
+        elif "pesquisar" in informacao:
+            falar("Qual é a pesquisa de hoje?")
+            termo = ouvir()
+            if termo != "Não entendi o que você disse":
+                resultado = pesquisar_google(termo)
+                falar(resultado)
+            else:
+                falar("Não entendi o que você quer pesquisar")
         else: 
             falar("Desculpe. Ainda não sei resolver isso.")
 
